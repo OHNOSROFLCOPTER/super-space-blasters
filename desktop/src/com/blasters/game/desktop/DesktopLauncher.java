3c370f419bcde8ebfdf457f9aa0b6c31a650a156
OHNOSROFLCOPTER <srymnir@gmail.com>
2016-10-28 18:44:05 +0000

    Initial commit

---
+++ desktop/src/com/blasters/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blasters.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blasters.game.SuperSpaceBlasters;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperSpaceBlasters(), config);
+	}
+}
